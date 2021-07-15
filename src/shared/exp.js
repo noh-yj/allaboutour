8676e2324761cc08055be5431b43a45dd2df73ec
noh-yj <goky456@gmail.com>
2021-07-15 14:57:17 +0900

    edit to comment

--- src/shared/exp.js
+++ src/shared/exp.js
@@ -1,13 +1,15 @@
 // 정규식 처리 생년월일
 
 // 생년월일 8자
+
 export const birthCheck = (birth) => {
   let reg = /^(19[0-9][0-9]|20\d{2})(0[0-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])$/;
 
   return reg.test(birth);
 };
 
-// copy url
+// copy url function
+
 export function copyToClipboard(textToCopy) {
   // navigator clipboard api needs a secure context (https)
   if (navigator.clipboard && window.isSecureContext) {
